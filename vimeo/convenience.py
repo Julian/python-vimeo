9120882fea8c2481650ecbf68870f42fa25cb055
MY <Julian@MishkanYerushalayim.com>
2010-08-05 21:48:15 -0400

    Uploading works (single chunk only, should chunk soon). Uses the bolacha wrapper for httplib2 which didn't support POST uploading out of the box. I rewrote a lot of the bolacha module to make it cleaner (see that repo) and will probably be doing more work on it to make uploading even easier.

--- vimeo/convenience.py
+++ vimeo/convenience.py
@@ -8,59 +8,83 @@ shouldn't completely break either until Vimeo seriously changes their API, keep
 in mind that if something in this module doesn't work, it still might work the
 "conventional" way using just the base module.
 """
+from os.path import getsize
 from urllib import urlencode
 
-import httplib2
+import urllib2
 import oauth2
 
-from . import VimeoClient
+from . import VimeoClient, VimeoError, API_REST_URL
+from bolacha import Bolacha as Http
 
 class VimeoUploader(object):
     """
     A convenience uploader class to be used alongside a client.
+
+    The ticket is assumed to be a dict-like object, which means that if you
+    aren't using a JSON client the ticket will need to be converted first.
     """
     def __init__(self, vimeo_client, ticket, **kwargs):
         self.vimeo_client = vimeo_client
         self.endpoint = ticket["endpoint"]
         self.ticket_id = ticket["id"]
+        self.max_file_size = ticket["max_file_size"]
         self.chunk_id = 0
 
         self.user = getattr(vimeo_client, "user", None)
 
-        quota = kwargs.pop("quota", None)
-        if quota:
-            self.has_sd_quota = bool(quota["sd_quota"])
-            self.has_hd_quota = bool(quota["hd_quota"])
-            self.upload_space = quota["upload_space"]
+        quota = kwargs.pop("quota", {})
+        self.has_sd_quota = bool(quota.get("sd_quota", None))
+        self.has_hd_quota = bool(quota.get("hd_quota", None))
+        self.upload_space = quota.get("upload_space", {})
+
+    def _check_file_size(self, file_size):
+        if file_size > self.upload_space.get("free", file_size):
+            raise VimeoError("Not enough free space to upload the file.")
+        elif file_size > self.max_file_size:
+            raise VimeoError("File is larger than the maximum allowed size.")
 
     def _post_to_endpoint(self, file_data, **kwargs):
-        params = {"ticket_id" : self.ticket_id,
-                  "chunk_id" : self.chunk_id}
+        params = {"chunk_id" : self.chunk_id,
+                  "ticket_id" : self.ticket_id}
 
-        method, consumer, token = (self.vimeo_client.signature_method,
-                                   self.vimeo_client.consumer,
-                                   self.vimeo_client.token)
-
-        request = oauth2.Request.from_consumer_and_token(consumer=consumer,
-                                                       token=token,
-                                                       http_method="POST",
-                                                       http_url=self.endpoint,
-                                                       parameters=params)
-        request.sign_request(method, consumer, token)
-
-        # file data should be unsigned as per the API docs
-        body = "{0}&file_data={1}".format(request.to_postdata(), file_data)
         headers = kwargs.get("headers",
                              dict(self.vimeo_client._CLIENT_HEADERS))
-        headers.setdefault("Content-Type", "application/x-www-form-urlencoded")
 
-        return httplib2.Http.request(self.vimeo_client.client, self.endpoint,
-                                   method="POST", body=body, headers=headers,
-                                   redirections=httplib2.DEFAULT_MAX_REDIRECTS,
-                                   connection_type=None)
+        request = oauth2.Request.from_consumer_and_token(
+                                          consumer=self.vimeo_client.consumer,
+                                          token=self.vimeo_client.token,
+                                          http_method="POST",
+                                          http_url=self.endpoint,
+                                          parameters=params)
 
-    def upload(self, file_data):
-        _, content = self._post_to_endpoint(file_data)
+        request.sign_request(self.vimeo_client.signature_method,
+                             self.vimeo_client.consumer,
+                             self.vimeo_client.token)
+
+        # httplib2 doesn't support uploading out of the box, so use our wrap
+        return Http().request_with_files(url=self.endpoint,
+                                         method="POST",
+                                         body=request,
+                                         body_files={"file_data" : file_data},
+                                         headers=headers)
+
+    def upload(self, file_path, chunk=False, chunk_size=2*1024*1024,
+               chunk_complete_hook=None):
+        """
+        Performs the steps of an upload. Checks file size and can handle
+        splitting into chunks.
+        """
+
+        file_size = getsize(file_path)
+        self._check_file_size(file_size)
+
+        if chunk:
+            pass
+        else:
+            pass
+        self._post_to_endpoint(open(file_path))
         self.chunk_id += 1
+
         return self.vimeo_client.vimeo_videos_upload_verifyChunks(
                                                 ticket_id=self.ticket_id)
